0630e1d256452b0ece85404d74ceeb2de88db01b
nmetens <metens@pdx.edu>
2025-04-25 09:48:48 -0700

    Famous Quote servered

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+async fn serve() -> Result<(), Box<dyn std::error::Error>> {
+    // Set up the address:
+    let localhost = "127.0.0.1";
+    let port = "8000";
+    let address = format!("{}:{}", localhost, port);
+
+    // Set up the server:
+    let listener = TcpListener::bind(address).await?;
+ 
+    let app = Router::new().route("/", get(|| async {"Famous Quote"}));
+
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    serve().await.expect("No famous quote found");
 }
